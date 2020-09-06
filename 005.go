a729e6d002f0696c2959de13b90d2f020ba4621b
Dan Neff <dan@neff.cc>
2020-09-06 08:24:29 -0700

    placeholder

---
+++ 005.go
@@ -0,0 +1,19 @@
+package main
+
+/*
+2520 is the smallest number that can be divided by each of the numbers
+from 1 to 10 without any remainder.
+
+What is the smallest positive number that is evenly divisible by all
+of the numbers from 1 to 20?
+*/
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("What is the smallest positive number that is evenly divisible by all")
+	fmt.Println("of the numbers from 1 to 20?")
+
+}
